97f68c1bbc94002e884e57d9930c34cc26ea950e
Jethro Landa <jemaq_lan@hotmail.com>
2025-11-09 20:07:25 +0800

    team events and points summary shortcode

--- includes/shortcode.class.php
+++ includes/shortcode.class.php
@@ -23,6 +23,8 @@ class Shortcode
   {
     add_shortcode('player_lineup', array($this, 'player_lineup'));
     add_shortcode('team_ladder', array($this, 'team_ladder'));
+    add_shortcode('team_events', array($this, 'team_events'));
+    add_shortcode('points_summary', array($this, 'points_summary'));
   }
 
   /**
@@ -57,7 +59,7 @@ class Shortcode
     $data = $this->getPlayerLineUpData($args);
 
     if (!empty($data['allMatchStatsSummary'])) {
-      require_once(REA_VIEWS_ROOT_DIR . 'player-lineup-view.php');
+      require(REA_VIEWS_ROOT_DIR . 'player-lineup-view.php');
     }
 
 
@@ -320,17 +322,19 @@ class Shortcode
   public function team_ladder($atts)
   {
     $atts = shortcode_atts(array(
+      'id' => uniqid(),
       'season_id' => '',
       'competition_id' => ''
     ), $atts, 'team_ladder');
 
-    extract($atts);
+    $competition_id = $atts['competition_id'];
 
-    $terms = get_the_terms(get_the_ID(), 'sp_league');
-    if (!empty($terms)) {
-      $competition_id = get_term_meta($terms[0]->term_id, 'competition_id', true);
+    if (empty($competition_id)) {
+      $terms = get_the_terms(get_the_ID(), 'sp_league');
+      if (!empty($terms)) {
+        $competition_id = get_term_meta($terms[0]->term_id, 'competition_id', true);
+      }
     }
-
 
     ob_start();
 
@@ -340,7 +344,7 @@ class Shortcode
     $data = $this->getCompetitionLadderData($args);
 
     if (!empty($data['ladderPools'])) {
-      require_once(REA_VIEWS_ROOT_DIR . 'team-ladder-view.php');
+      require(REA_VIEWS_ROOT_DIR . 'team-ladder-view.php');
     }
 
 
@@ -410,5 +414,178 @@ class Shortcode
       // print_r($data);
       // echo '</pre>';
     }
+  }
+
+  public function team_events($atts)
+  {
+    $atts = shortcode_atts(array(
+      'id' => uniqid(),
+      'entity_id' => '',
+      'season' => '',
+      'competition_id' => '',
+      'team_id' => ''
+    ), $atts, 'team_ladder');
+
+    ob_start();
+
+    $data = $this->getTeamCompetitionEventsData($atts);
+
+    if (!empty($data)) {
+      require(REA_VIEWS_ROOT_DIR . 'team-events-view.php');
+    }
+
+    // content
+    return ob_get_clean();
+  }
+
+  public function getTeamCompetitionEventsData($args)
+  {
+    // args example
+    // $args = array(
+    //   'season' => '2025',
+    //   'competition' => 'mLGoqgHnacX2AnmgD',
+    //   'team' => 'DZJhdynaY4wSDBQpQ',
+    //   'entityId' => '53371',
+    //   'type' => 'fixtures' or 'results'
+    // );
+    extract($args);
+
+    $body = [
+      "operationName" => "EntityFixturesAndResults",
+      "variables" => [
+        "season" => $season,
+        "comps" => [
+          [
+            "id" => $competition_id,
+            "sourceType" => "2"
+          ]
+        ],
+        "teams" => [$team_id],
+        "type" => 'results',
+        "skip" => 0,
+        "limit" => 100,
+        "entityId" => (int)$entity_id,
+        "entityType" => "club"
+      ],
+      "query" => "query EntityFixturesAndResults(\$entityId: Int, \$entityType: String, \$season: String, \$comps: [CompInput], \$teams: [String], \$type: String, \$skip: Int, \$limit: Int) {
+      getEntityFixturesAndResults(
+        season: \$season
+        comps: \$comps
+        teams: \$teams
+        entityId: \$entityId
+        entityType: \$entityType
+        type: \$type
+        limit: \$limit
+        skip: \$skip
+      ) {
+        ...Fixtures_fixture
+        __typename
+      }
+    }
+
+    fragment Fixtures_fixture on FixtureItem {
+      id
+      compId
+      compName
+      dateTime
+      group
+      isLive
+      isBye
+      round
+      roundType
+      roundLabel
+      season
+      status
+      venue
+      sourceType
+      matchLabel
+      homeTeam {
+        ...Fixtures_team
+        __typename
+      }
+      awayTeam {
+        ...Fixtures_team
+        __typename
+      }
+      fixtureMeta {
+        ...Fixtures_meta
+        __typename
+      }
+      __typename
+    }
+
+    fragment Fixtures_team on Team {
+      id
+      name
+      teamId
+      score
+      crest
+      __typename
+    }
+
+    fragment Fixtures_meta on Fixture {
+      id
+      ticketURL
+      ticketsAvailableDate
+      isSoldOut
+      radioURL
+      radioStart
+      radioEnd
+      streamURL
+      streamStart
+      streamEnd
+      broadcastPartners {
+        ...Fixtures_broadcastPartners
+        __typename
+      }
+      __typename
+    }
+
+    fragment Fixtures_broadcastPartners on BroadcastPartner {
+      id
+      name
+      link
+      photoId
+      __typename
+    }"
+    ];
+
+    $response = wp_remote_post('https://rugby-au-cms.graphcdn.app/', [
+      'headers' => [
+        'Content-Type' => 'application/json',
+      ],
+      'body' => wp_json_encode($body),
+      'method' => 'POST',
+      'data_format' => 'body',
+    ]);
+
+    if (is_wp_error($response)) {
+      error_log('GraphQL Error: ' . $response->get_error_message());
+    } else {
+      $data = json_decode(wp_remote_retrieve_body($response), true);
+      $results = $data['data']['getEntityFixturesAndResults'];
+      return $results;
+    }
+  }
+
+  public function points_summary($atts)
+  {
+    $atts = shortcode_atts(array(
+      'id' => uniqid(),
+    ), $atts, 'points_summary');
+
+    ob_start();
+
+    $args = array(
+      'fixture_id' => get_post_meta(get_the_ID(), 'fixture_id', true)
+    );
+    $data = $this->getPlayerLineUpData($args);
+
+    if (!empty($data)) {
+      require(REA_VIEWS_ROOT_DIR . 'points-summary-view.php');
+    }
+
+    // content
+    return ob_get_clean();
   }
 }
