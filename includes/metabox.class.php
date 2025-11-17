580c7584442269363abd3e5d936a40cf5bf1375c
Jethro Landa <jemaq_lan@hotmail.com>
2025-11-17 23:09:42 +0800

    option to update player id

--- includes/metabox.class.php
+++ includes/metabox.class.php
@@ -26,6 +26,12 @@ class Metabox
 
     // 3️⃣ Save the meta box data
     add_action('save_post_sp_event', array($this, 'save_game_fixture_id_metabox'));
+
+    // 1️⃣ Add the meta box Player ID
+    add_action('add_meta_boxes', array($this, 'add_player_id_metabox'));
+
+    // 3️⃣ Save the meta box data
+    // add_action('save_post_sp_event', array($this, 'save_player_id_metabox'));
   }
 
   /**
@@ -40,6 +46,7 @@ class Metabox
     }
     return self::$_instance;
   }
+
   public function add_game_fixture_id_metabox()
   {
     add_meta_box(
@@ -74,7 +81,7 @@ class Metabox
         placeholder="e.g. bd555ab34f689975d" />
     </p>
 
-<?php
+  <?php
   }
 
   public function save_game_fixture_id_metabox($post_id)
@@ -97,6 +104,66 @@ class Metabox
     if (isset($_POST['fixture_id'])) {
 
       update_post_meta($post_id, 'fixture_id', sanitize_text_field($_POST['fixture_id']));
+    }
+  }
+
+  public function add_player_id_metabox()
+  {
+    add_meta_box(
+      'player_id',            // ID
+      'Player ID',                 // Title
+      array($this, 'render_player_id'),     // Callback
+      'sp_player',                   // Post type slug (change this)
+      'normal',                     // Context ('normal', 'side', 'advanced')
+      'default'                     // Priority
+    );
+  }
+
+  // 2️⃣ Render the meta box HTML
+  public function render_player_id()
+  {
+    // Security nonce
+    wp_nonce_field('save_player_id', 'player_id_nonce');
+
+    // Get saved values
+    global $post;
+    $player_id = get_post_meta($post->ID, 'player_id', true);
+  ?>
+
+    <p>
+      <label for="player_id"><strong>Player ID:</strong></label><br>
+      <input
+        type="text"
+        id="player_id"
+        name="player_id"
+        value="<?php echo esc_attr($player_id); ?>"
+        style="width:100%;"
+        placeholder="e.g. DkJm85qoaBLDopyiG__11" />
+    </p>
+
+<?php
+  }
+
+  public function save_player_id_metabox($post_id)
+  {
+
+    // Verify nonce
+    if (
+      !isset($_POST['player_id_nonce']) ||
+      !wp_verify_nonce($_POST['player_id_nonce'], 'save_player_id')
+    ) {
+      return;
+    }
+
+    // Check autosave or permissions
+    if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) return;
+    if (!current_user_can('edit_post', $post_id)) return;
+
+
+    // Sanitize & save
+    if (isset($_POST['player_id'])) {
+
+      update_post_meta($post_id, 'player_id', sanitize_text_field($_POST['player_id']));
     }
   }
 }
